70d0ba2baa9872ab0f5299a30fa6c48827e9bd54
liuzuan <467209099@qq.com>
2018-07-07 09:21:56 +0800

    sidebsr style

--- app/dev/js/index.js
+++ app/dev/js/index.js
@@ -1,19 +1,11 @@
 $(document).ready(() => {
+    let $body = $('body');
     $(document).delegate('.menu_icon', 'click', () => {
-        $('.sidebar').addClass('open');
+        $body.toggleClass('open');
     });
-    $('.sidebar').click(() => {
-        event.stopPropagation();
-    });
-    
-    $('body').click(() => {
+    $(window).resize(() => {
         if ($(window).innerWidth() < 900) {
-            $('.sidebar').removeClass('open');
+            $body.removeClass('open');
         }
     });
-    $(window).resize(()=> {
-        if ($(window).innerWidth() < 900) {
-            $('.sidebar').removeClass('open');
-        }
-    })
 });
--- app/routes/message.js
+++ app/routes/message.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('pages/index', {
+    res.render('pages/message', {
         active_nav: 'message',
     });
 });
