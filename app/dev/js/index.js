8942d2cadd59344ff4f59ba28a910d8b6f1ae062
liuzuan <467209099@qq.com>
2018-07-07 10:17:42 +0800

    css autoprefixer

--- app/dev/js/index.js
+++ app/dev/js/index.js
@@ -1,8 +1,26 @@
 $(document).ready(() => {
-    let $body = $('body');
-    $(document).delegate('.menu_icon', 'click', () => {
+    const $body = $('body');
+    const $search_input = $('.search_input');
+    const $reset = $(".reset_search_btn");
+    const $menu_icon = $(".menu_icon");
+
+    $menu_icon.click(function(){
         $body.toggleClass('open');
-    });
+    })
+
+    $search_input.on('input change',function(){
+        if ($(this).val() === '') {
+            $reset.fadeOut(300);
+        } else {
+            $reset.fadeIn(300);
+        }
+    })
+
+    $reset.on('click',function(){
+        $search_input.val('');
+        $reset.fadeOut(300);
+    })
+
     $(window).resize(() => {
         if ($(window).innerWidth() < 900) {
             $body.removeClass('open');
--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,33 @@ var $ = require('gulp-load-plugins')({
 });
 var minifyCss = require('gulp-minify-css');
 
+function swallowError(error) {
+    console.error(error.toString());
+    this.emit('end');
+}
+
 gulp.task('lessdev', function() {
-    gulp
-        .src(['app/dev/less/*.less','!app/dev/less/common.less', '!app/dev/less/mixin.less'])
+    gulp.src(['app/dev/less/*.less', '!app/dev/less/common.less', '!app/dev/less/mixin.less'])
         .pipe($.less())
+        .pipe(
+            $.autoprefixer({
+                browsers: ['last 3 Safari versions','last 2 Explorer versions', 'iOS 7','Firefox >= 20'],
+                cascade: false
+            })
+        )
+        .on('error', swallowError)
         .pipe(minifyCss())
         .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('jsdev', function() {
-    gulp
-        .src('app/dev/js/*.js')
+    gulp.src('app/dev/js/*.js')
         .pipe(
             $.babel({
                 presets: ['env']
             })
         )
+        .on('error', swallowError)
         .pipe($.uglify())
         .pipe(gulp.dest('public/js/'));
 });
