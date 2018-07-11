42ee40a2fbe5f77c891b738cc8406a72d9b30066
liuzuan <467209099@qq.com>
2018-07-12 00:27:24 +0800

    mod port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 router(app);
 
