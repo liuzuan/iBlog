ddd034d336e208b7b7e793b445102c13403b7ba0
liuzuan <467209099@qq.com>
2018-07-12 13:12:29 +0800

    mod filePath

--- app.js
+++ app.js
@@ -25,9 +25,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+// app.use(express.static('public'));
 // app.use(express.static(__dirname + '/public'));
-// app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
+
 
 console.log(__dirname)
 
