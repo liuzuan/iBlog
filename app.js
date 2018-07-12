2be2010656808dfa29a64c70db805615a54fe8fa
liuzuan <467209099@qq.com>
2018-07-12 13:37:45 +0800

    mod filePath

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ app.use(
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'pug');
 
-router(app);
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/public')));
+router(app);
 
 
 // catch 404 and forward to error handler
