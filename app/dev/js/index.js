9c2b84c9d9f7af4301b388379da7ce80fa056241
liuzuan <467209099@qq.com>
2018-08-04 17:10:44 +0800

    Judge the browser

--- app/dev/js/index.js
+++ app/dev/js/index.js
@@ -9,38 +9,33 @@ $(function() {
     browserType();
 
     function browserType() {
-        var browser = {
-            versions: (function() {
-                var u = navigator.userAgent,
-                    app = navigator.appVersion;
-                return {
-                    // 移动终端浏览器版本信息
-                    trident: u.indexOf('Trident') > -1, // IE内核
-                    presto: u.indexOf('Presto') > -1, // Opera内核
-                    webKit: u.indexOf('AppleWebKit') > -1, // 苹果、谷歌内核
-                    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, // 火狐内核
-                    mobile:
-                        !!u.match(/AppleWebKit.*Mobile.*/) ||
-                        (!!u.match(/AppleWebKit/) && u.indexOf('QIHU') && u.indexOf('Chrome') < 0), // 是否为移动终端
-                    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), // iOS终端
-                    android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, // Android 终端或者 UC 浏览器
-                    iPhone: u.indexOf('iPhone') > -1 || u.indexOf('Mac') > -1, // 是否为 iPhone 或者 QQHD 浏览器
-                    iPad: u.indexOf('iPad') > -1, // 是否 iPad
-                    webApp: u.indexOf('Safari') == -1, // 是否WEB应用程序，没有头部与底部。
-                    ua: u
-                };
-            })(),
-
-            language: (navigator.browserLanguage || navigator.language).toLowerCase()
-        };
-
-        if (browser.versions.mobile || browser.versions.iPad) {
+        var t0 = new Date();
+        var os = (function() {
+            var ua = navigator.userAgent,
+                isWindowsPhone = /(?:Windows Phone)/.test(ua),
+                isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+                isAndroid = /(?:Android)/.test(ua),
+                isFireFox = /(?:Firefox)/.test(ua),
+                isChrome = /(?:Chrome|CriOS)/.test(ua),
+                isTablet =
+                    /(?:iPad|PlayBook)/.test(ua) ||
+                    (isAndroid && !/(?:Mobile)/.test(ua)) ||
+                    (isFireFox && /(?:Tablet)/.test(ua)),
+                isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+                isPc = !isPhone && !isAndroid && !isSymbian;
+            return {
+                isTablet: isTablet,
+                isPhone: isPhone,
+                isAndroid: isAndroid,
+                isPc: isPc
+            };
+        })();
+        if (os.isAndroid || os.isPhone || os.isTablet) {
             tap = 'touchstart';
         } else {
             tap = 'click';
         }
     }
-
     $menu_icon.on(tap, function() {
         $sidebar.css('transition', 'background 0.35s linear, height 0.35s ease-in');
         $body.toggleClass('open');
