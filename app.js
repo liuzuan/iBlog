d68e699cef44105dca521f18da655040696a76d4
liuzuan <467209099@qq.com>
2018-08-09 20:14:59 +0800

    ''

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
     cors({
-        origin: [app.get('env') === 'development' ? 'http://localhost:8081' : ''],
+        origin: '*',
+        // origin: [app.get('env') === 'development' ? 'http://localhost:8081' : ''],
         optionsSuccessStatus: 200,
         credentials: true
     })
