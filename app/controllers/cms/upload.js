3d74f5df6de60bc387ca066bab847323f7d27d89
liuzuan <467209099@qq.com>
2018-08-10 17:39:46 +0800

    update cms & add upload2cdn

--- app/controllers/cms/upload.js
+++ app/controllers/cms/upload.js
@@ -2,10 +2,11 @@
 import qiniu from 'qiniu';
 import formidable from 'formidable';
 import fs from 'fs';
-import path from 'path';
 import { galleryModel } from '../../models/';
+
 qiniu.conf.ACCESS_KEY = 'HWBhm1kwS_YY-bHTR9Cl2gWr20eSks14PhIaTqP7';
 qiniu.conf.SECRET_KEY = 'lveC7RDx27R7NzTMtBNM3BxYKbsPao-j8sPnF7KT';
+
 class uploadController {
     async uploadToCdn(req, res, next) {
         try {
@@ -13,23 +14,18 @@ class uploadController {
             const bucket = 'liuzuanncdn';
             form.uploadDir = 'public/upload/';
             form.keepExtensions = true;
+            form.maxFieldsSize = 5 * 1024 * 1024;
             function uploadFile(uptoken, key, localFile, title) {
                 const extra = new qiniu.io.PutExtra();
-                const url = 'http://cdn.liuzuann.com/' + ret.key;
-                qiniu.io.putFile(uptoken, key, localFile, extra, async(err, ret) => {
-                    if (req.query) {
+                qiniu.io.putFile(uptoken, key, localFile, extra, async (err, ret) => {
+                    const url = 'http://cdn.liuzuann.com/' + ret.key;
+                    if (req.query && req.query.gallery) {
                         const img = new galleryModel({
                             title: title.slice(0, title.indexOf('.')),
                             thumbnail: url + '-thumb',
                             full: url + '-thin'
                         });
-                        await img.save((err, data) => {
-                            res.send({
-                                success: true,
-                                name: ret.key,
-                                url: url + '-thin'
-                            });
-                        });
+                        await img.save();
                     }
                     res.send({
                         success: true,
@@ -38,20 +34,20 @@ class uploadController {
                     });
                 });
             }
-            form.parse(req, (err, fields, files) => {
-                const imgName = (new Date().getTime() + Math.ceil(Math.random() * 10000)).toString(16);
-                const basename = path.basename(files.file.name);
-                const key = imgName + '_' + basename;
-                const repath = 'public/upload/' + key;
-                fs.rename(files.file.path, repath);
+            form.parse(req, async (err, fields, files) => {
+                const uniqueName = (new Date().getTime() + Math.ceil(Math.random() * 10000)).toString(16);
+                const originalname = files.file.name;
+                const key = uniqueName + '__' + originalname;
+                const repath = 'public/upload/' + originalname;
+                fs.renameSync(files.file.path, repath);
                 let putPolicy = new qiniu.rs.PutPolicy(bucket + ':' + key);
                 const token = putPolicy.token();
-                uploadFile(token.toString(), key, repath, basename);
-                fs.unlink(repath);
+                uploadFile(token.toString(), key, repath, originalname);
+                fs.unlinkSync(repath);
             });
         } catch (err) {
             console.log('上传七牛失败-->', err);
-            fs.unlink(repath);
+            fs.unlinkSync(repath);
             res.send({
                 success: false,
                 desc: '上传失败'
@@ -61,7 +57,7 @@ class uploadController {
 
     // uploadHandle(req, res, next) {
     //     // console.log(req.file);
-    //     let uploadDir = 'public/img/article/';
+    //     const uploadDir = 'public/upload/';
 
     //     const storage = multer.diskStorage({
     //         destination: function(req, file, cb) {
