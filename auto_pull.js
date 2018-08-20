c055c4075e9d971104398f763f6ec1d0bd2977aa
liuzuan <467209099@qq.com>
2018-08-20 17:40:25 +0800

    remove pre's style 'max-height'

--- auto_pull.js
+++ auto_pull.js
@@ -1,7 +1,6 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/auto_pull', secret: 'auto_pull' }) 
-// 上面的 secret 保持和 GitHub 后台设置的一致
  
 function run_cmd(cmd, args, callback) {
   var spawn = require('child_process').spawn;
