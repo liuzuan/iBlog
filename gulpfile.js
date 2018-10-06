7ce04d1fbeab323822f78f6f5967027e91b3bc12
liuzuan <467209099@qq.com>
2018-10-06 12:50:43 +0800

    ''

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 var gulp = require('gulp');
-
 var $ = require('gulp-load-plugins')({
     pattern: ['gulp-*']
 });
 var minifyCss = require('gulp-minify-css');
-
+var browserSync = require('browser-sync').create();
 function swallowError(error) {
     console.error(error.toString());
     this.emit('end');
@@ -15,13 +14,14 @@ gulp.task('lessdev', function() {
         .pipe($.less())
         .pipe(
             $.autoprefixer({
-                browsers: ['last 3 Safari versions','last 2 Explorer versions', 'iOS 7','Firefox >= 20'],
+                browsers: ['last 3 Safari versions', 'last 2 Explorer versions', 'iOS 7', 'Firefox >= 20'],
                 cascade: false
             })
         )
         .on('error', swallowError)
         .pipe(minifyCss())
-        .pipe(gulp.dest('public/css/'));
+        .pipe(gulp.dest('public/css/'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('jsdev', function() {
@@ -33,10 +33,19 @@ gulp.task('jsdev', function() {
         )
         .on('error', swallowError)
         .pipe($.uglify())
-        .pipe(gulp.dest('public/js/'));
+        .pipe(gulp.dest('public/js/'))
+        .pipe(browserSync.reload({stream: true}));
+
 });
 
-gulp.task('watch', function() {
+gulp.task('server', function() {
+    browserSync.init({
+        proxy: 'http://localhost:3000',
+        files: ['app/views/**/*.pug', 'public/js/*.js', 'public/css/*.css']
+    });
     gulp.watch('app/dev/less/*.less', ['lessdev']);
     gulp.watch('app/dev/js/*.js', ['jsdev']);
+    gulp.watch('app/views/**/*.pug').on('change', browserSync.reload);
 });
+
+gulp.task('default', ['server']);
